76b68467cbec92dc34e22813bcebfb1ea0ec62e8
Alexander Richey <alexander.richey@gmail.com>
2020-04-08 01:28:48 -0700

    remove from rsvps when user leaves event

--- handlers/event.go
+++ handlers/event.go
@@ -416,6 +416,11 @@ func RemoveUserFromEvent(w http.ResponseWriter, r *http.Request) {
 	// If the requestor is the owner or the requestor is the user to be
 	// removed, then remove the user.
 	if event.OwnerIs(&u) || userToBeRemoved.Key.Equal(u.Key) {
+		if err := event.RemoveRSVP(&u); err != nil {
+			bjson.HandleError(w, err)
+			return
+		}
+
 		if err := event.RemoveUser(&userToBeRemoved); err != nil {
 			bjson.HandleError(w, err)
 			return
